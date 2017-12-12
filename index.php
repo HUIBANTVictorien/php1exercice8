41b454c48e04e533fc26ef5edbcf0722543d419b
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:09:38 +0100

    maj exo8

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php
-$addition = 3 + 4;
-$multiplication = 5 * 20;
-$division = 45 / 5;
-echo $addition.'<br/>';
-echo $multiplication.'<br/>';
-echo $division.'<br/>';
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 8 parti 1 php</title>
+  </head>
+  <body>
+    <?php
+    $addition = 3 + 4;
+    $multiplication = 5 * 20;
+    $division = 45 / 5;
+    echo $addition.'<br/>';
+    echo $multiplication.'<br/>';
+    echo $division.'<br/>';
+    ?>
+  </body>
+</html>
